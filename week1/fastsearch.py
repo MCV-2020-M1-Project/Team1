d96f7eee762ad38155124702f1d5db37656f998a
adityassrana <adityassrana2@gmail.com>
2020-10-16 19:44:50 +0530

    final upload

--- week1/fastsearch.py
+++ week1/fastsearch.py
@@ -26,6 +26,18 @@ def get_image_path_list(data_path:str) -> List[Path]:
     """
     return sorted(glob.glob(os.path.join(data_path,'*.jpg')))
 
+def get_image_mask_list(data_path:str) -> List[Path]:
+    """
+    Used for indexing all masks in a folder
+    
+    Args:
+        data_path: path to folder containing the images
+    
+    Returns: 
+        a list of paths to all the images in the folder
+    """
+    return sorted(glob.glob(os.path.join(data_path,'*.png')))
+
 def path2img(path:Path) -> np.ndarray:
     """
     Convert image to numpy array from path
@@ -37,6 +49,20 @@ def path2img(path:Path) -> np.ndarray:
         a numpy array image from specified path
     """
     return cv2.imread(path)
+
+def path2mask(path:Path) -> np.ndarray:
+    """
+    Convert image to numpy array from path
+    
+    Args: 
+        path: path to image
+
+    Returns: 
+        a numpy array image from specified path
+    """
+    mask = cv2.imread(path)
+    gray_mask = cv2.cvtColor(mask,cv2.COLOR_BGR2GRAY)
+    return gray_mask
 
 def path2img_np(path:str) -> np.ndarray:
     """
@@ -70,7 +96,7 @@ def get_ground_truth(data_path:str=None) -> List[List[int]]:
     Retriev pickle file containing ground truths
     of query images
     """
-    gt_file_list = glob.glob(os.path.join(data_path,'*.pkl'))
+    gt_file_list = glob.glob(os.path.join(data_path,'*gt*.pkl'))
     with open(gt_file_list[0], 'rb') as f:
         query_gt = pickle.load(f)
     return query_gt
@@ -168,7 +194,7 @@ def parse_args(args=sys.argv[1:]):
         help = "path to reference museum dataset. Example input: 'data/BBDD'")
 
     parser.add_argument(
-        "--query_path","-q", default="/home/adityassrana/datatmp/Datasets/museum_dataset/processed/qsd1_w1",
+        "--query_path","-q", default="/home/adityassrana/datatmp/Datasets/museum_dataset/processed/qsd2_w1",
         help = "path to query museum dataset. Example input: 'data/qsd1_w1'")
 
     parser.add_argument(
@@ -193,6 +219,50 @@ def parse_args(args=sys.argv[1:]):
     args = parser.parse_args(args)
     return args
 
+def search_batch_mask(query_list:List[Path]=None, descriptor:str='rgb_histogram_1d', metric:str="euclidean", bins:int=64, k:int=10) -> List[int]:
+    """
+    Searches the reference dataset for most similar images to the
+    query image
+
+    Args:
+        query_list: list containing path to all query images
+        descriptor: method used to compute features
+        metric: similarity measure to compare images
+        bins: number of bins to use for histogram
+        k: to return top k images that are most similar
+
+    Returns:
+        a list of lists indices of top k images in reference dataset
+        that are most similar to query image
+    """
+    
+    # define particular descriptors and distance metrics to be used
+    # so that you don't have to input the function arguments every 
+    #time you call the function
+    extract_features_func = partial(extract_features, descriptor=descriptor,bins=bins)
+
+    distance_func = partial(compute_distance, metric=metric)
+    
+    def _extract_features_from_path(_path):
+        return extract_features_func(path2img(_path))
+
+    def _extract_features_from_path_mask(_path,_path_mask):
+        return extract_features_func(path2img(_path), mask=path2mask(_path_mask))
+
+    result_list_of_lists = []
+    with mp.Pool(processes=20) as p:
+        
+        query_descriptors = p.starmap(_extract_features_from_path_mask, [(query_path,mask_path) for query_path, mask_path in zip(query_list, mask_list)])
+        image_descriptors = p.map(_extract_features_from_path, [ref_path for ref_path in museum_list])
+    
+        for q in range(len(query_list)):
+            query_feature = query_descriptors[q]
+            dist = p.starmap(distance_func, [(query_feature, ref_feature) for ref_feature in image_descriptors])
+            nearest_indices = np.argsort(dist)[:k]
+            result_list = [index for index in nearest_indices]
+            result_list_of_lists.append(result_list)
+    return result_list_of_lists
+
 if __name__ == '__main__':
     args = parse_args()
     print(args)
@@ -200,22 +270,26 @@ if __name__ == '__main__':
     #path to data
     museum_data = args.museum_path
     query_data = args.query_path
+    print(query_data)
     
     #indexing the images and ground truth
     museum_list = get_image_path_list(museum_data)
     query_list = get_image_path_list(query_data)
     ground_truth= get_ground_truth(query_data)
 
+    mask_list = get_image_mask_list(query_data)
+
     print(f'number of images in reference dataset is {len(museum_list)}')
     print(f'number of images in query dataset is {len(query_list)}')
+    print(f'number of masks in query dataset is {len(query_list)}')
 
     #results = search(query_list[6],k=10)
     #print(f'Most similar images in the reference dataset are {results}')
 
     k = args.map_k
     print('---------Now Searching Batches--------')
-    result_list_of_lists = search_batch(query_list = query_list, descriptor = args.descriptor, metric = args.metric, bins= args.bins, k=k)
-
+    result_list_of_lists = search_batch_mask(query_list = query_list, descriptor = args.descriptor, metric = args.metric, bins= args.bins, k=k)
+    print(result_list_of_lists)
     map_k = mapk(ground_truth, result_list_of_lists, k=k)
     print(f'map@{k} for the current run is {map_k}')
 
